3e3e4c7db547e6d65996c5d98c12e15dc96f4f29
Mohammad Ali <itsproali@gmail.com>
2022-03-10 22:52:48 +0600

    Repor Section Fixed

--- js/app.js
+++ js/app.js
@@ -39,9 +39,10 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
-
+    
     displayLikedPosts();
   } else {
+    document.getElementById("reported").innerHTML = '';
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
@@ -150,7 +151,7 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
